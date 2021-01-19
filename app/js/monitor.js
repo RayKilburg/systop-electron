b4b91bbaee5ab80131967cb432d50940afbe5ad0
RayKilburg <rkilburg42@gmail.com>
2021-01-18 19:19:18 -0500

    Notifications API

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -6,6 +6,12 @@ const os = osu.os
 
 let cpuOverload = 80
 
+notifyUser({
+    title: 'CPU overload',
+    body: `CPU is over ${cpuOverload}%`,
+    icon: path.join(__dirname, 'img', 'icon.png')
+})
+
 // Run every 2 seconds
 setInterval(() => {
     // CPU Usage
@@ -53,4 +59,9 @@ function secondsToDhms(seconds) {
     const m = Math.floor((seconds % 3600) / 60)
     const s = Math.floor(seconds % 60)
     return `${d}d, ${h}h, ${m}m, ${s}s`
+}
+
+// Send notification
+function notifyUser(options) {
+    new Notification(options.title, options)
 }
