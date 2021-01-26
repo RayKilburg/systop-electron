4211659d84397bc9ded0b433b50fcf313078c831
RayKilburg <rkilburg42@gmail.com>
2021-01-25 22:34:44 -0500

    MainWindow Class

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const path = require('path')
-const { app, BrowserWindow, Menu, ipcMain, Tray } = require('electron')
+const { app, Menu, ipcMain, Tray } = require('electron')
 const log = require('electron-log')
 const Store = require('./Store')
-const { triggerAsyncId } = require('async_hooks')
+const MainWindow = require('./MainWindow')
 
 // Set env
 process.env.NODE_ENV = 'development'
@@ -25,24 +25,7 @@ const store = new Store({
 })
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    title: 'systop',
-    width: isDev ? 800 : 355,
-    height: 500,
-    icon: `${__dirname}/assets/icons/icon.png`,
-    resizable: isDev ? true : false,
-    show: false,
-    opacity: 0.99,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  })
-
-  if (isDev) {
-    mainWindow.webContents.openDevTools()
-  }
-
-  mainWindow.loadFile('./app/index.html')
+  mainWindow = new MainWindow('./app/index.html', isDev)
 }
 
 app.on('ready', () => {
